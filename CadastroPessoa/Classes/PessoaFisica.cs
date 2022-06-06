b13774c4cb108a72fd4c7e54431b113ee5a5db64
Renata-senai <94024541+Renata-senai@users.noreply.github.com>
2022-06-06 13:53:50 -0300

    SA2 - ER2

---
+++ CadastroPessoa/Classes/PessoaFisica.cs
@@ -0,0 +1,26 @@
+using CadastroPessoa.Interfaces;
+
+namespace CadastroPessoa.Classes
+{
+    public class PessoaFisica : Pessoa , IPessoaFisica
+    {
+
+        public string? Cpf { get; set; }
+
+        public string? dataNasc { get; set; }
+        
+        
+
+
+        
+        public override float PagarImposto(float rendimento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidarDataNasc(DateTime dataNasc)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CadastroPessoa/Program.cs
@@ -0,0 +1,11 @@
+﻿using CadastroPessoa.Classes;
+
+PessoaFisica novaPf = new PessoaFisica();
+
+novaPf.Nome = "Renata";
+
+Console.WriteLine(novaPf.Nome);
+
+Console.WriteLine($"Nome: {novaPf.Nome}");
+
+Console.WriteLine("Nome: " + novaPf.Nome);
